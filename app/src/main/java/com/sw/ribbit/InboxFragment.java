18f240a22fbb730395e7002be1e3bf48eff9f4ea
Sen Wang <hitbear518@qq.com>
2014-08-15 00:32:22 +0800

    8 14 home

--- app/src/main/java/com/sw/ribbit/InboxFragment.java
+++ app/src/main/java/com/sw/ribbit/InboxFragment.java
@@ -1,18 +1,23 @@
 package com.sw.ribbit;
 
 import android.app.ListFragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -46,17 +51,53 @@ public class InboxFragment extends ListFragment {
 
 				if (e == null) {
 					mMessages = messages;
-
-
 					String[] usernames = new String[mMessages.size()];
 					for (int i = 0; i < mMessages.size(); i++) {
 						usernames[i] = mMessages.get(i).getString(ParseConstants.KEY_SENDER_NAME);
 					}
-					ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-							android.R.layout.simple_list_item_1, usernames);
-					setListAdapter(adapter);
+					if (getListAdapter() == null) {
+						MessageAdapter adapter = new MessageAdapter(getActivity(), mMessages);
+						setListAdapter(adapter);
+					} else {
+						MessageAdapter adapter = (MessageAdapter) getListAdapter();
+						adapter.refill(mMessages);
+					}
 				}
 			}
 		});
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+
+		ParseObject message = mMessages.get(position);
+		String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+		ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+		Uri fileUri = Uri.parse(file.getUrl());
+
+		if (messageType.equals(ParseConstants.TYPE_IMAGE)) {
+			Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+			intent.setData(fileUri);
+			startActivity(intent);
+		} else {
+			Intent intent = new Intent(Intent.ACTION_VIEW, fileUri);
+			intent.setDataAndType(fileUri, "video/*");
+			startActivity(intent);
+		}
+
+		List<String> ids = message.getList(ParseConstants.KEY_RECIPIENT_IDS);
+
+		if (ids.size() == 1) {
+			message.deleteInBackground();
+		} else {
+			ids.remove(ParseUser.getCurrentUser().getObjectId());
+
+			ArrayList<String> idsToRemove = new ArrayList<String>();
+			idsToRemove.add(ParseUser.getCurrentUser().getObjectId());
+
+			message.removeAll(ParseConstants.KEY_RECIPIENT_IDS, idsToRemove);
+			message.saveInBackground();
+		}
 	}
 }
